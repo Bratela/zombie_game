6079d274194f0ccbae3fea427f9f5cb8c66e4c3b
ViktorijaShuklina <viktorija.suklina@gmail.com>
2020-05-08 20:20:22 +0300

    add images

--- game.py
+++ game.py
@@ -1,2 +1,25 @@
-import "pygame"
-//edit//
+import pygame
+import random
+import time
+
+pygame.init()
+
+screen = pygame.display.set_mode((1063, 598))
+
+#Background
+background = pygame.image.load('./images/fon.png')
+
+pygame.display.set_caption("Zombie")
+icon = pygame.image.load('./images/zombie.png')
+pygame.display.set_icon(icon)
+
+#game engine
+done = False
+while not done:
+    pygame.display.flip()
+    screen.blit (background,(0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+pygame.quit()
+
