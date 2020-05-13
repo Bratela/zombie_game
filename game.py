906a2ceb1f3152dc800b17974790334f88769d84
ViktorijaShuklina <viktorija.suklina@gmail.com>
2020-05-13 16:51:03 +0300

    cube

--- game.py
+++ game.py
@@ -4,7 +4,16 @@ import time
 
 pygame.init()
 
-screen = pygame.display.set_mode((1063, 598))
+clock = pygame.time.Clock()
+
+display_width = 1000
+display_height = 600
+display = pygame.display.set_mode((display_width, display_height))
+
+zombie_width = 64
+zombie_height = 64
+zombie_x = display_width - 150
+zombie_y = display_height - zombie_height - 120
 
 #Background
 background = pygame.image.load('./images/fon.png')
@@ -17,10 +26,15 @@ pygame.display.set_icon(icon)
 done = False
 while not done:
     pygame.display.flip()
-    screen.blit (background,(0,0))
+    display.blit (background,(0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+
+    pygame.draw.rect(display, (122, 132, 253), (zombie_x, zombie_y, zombie_width, zombie_height))
+
+    pygame.display.update()
+    clock.tick(60)
 
 pygame.quit()
 
